9565d27f587364de77fa2c3c7f2b09ac6840e49b
Marcel Morawiec <moro_pl@wp.pl>
2020-03-09 18:05:27 +0100

    Changed text in APP

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Already lerned
         </a>
       </header>
     </div>
